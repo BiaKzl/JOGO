1efd67b29d0ceabe0e9f149a8ff4d75be07ed682
biakzl <biababinski@hotmail.com>
2024-10-07 08:29:53 -0300

    atualizações do projeto

--- player.gd
+++ player.gd
@@ -3,20 +3,44 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-const GRAVITY = 500.0  # Definindo a constante de gravidade
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
-	# Apply gravity
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity.y += gravity * delta
 
-	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get input direction (left/right)
-	var direction := Input.get_axis("ui_left", "ui_right")
-	velocity.x = direction * SPEED
+	# Get the input direction and handle the movement/deceleration.
+	# As a good practice, you should replace UI actions with custom gameplay action names.
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	#flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0 :
+		animated_sprite. flip_h = true
+		
+	if is_on_floor() :
+		if direction == 0 :
+			animated_sprite.play("idle")
+		else	:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	
 
-	# Move the player (CharacterBody2D move_and_slide doesn't require arguments in Godot 4.x)
+#apply moviment	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
